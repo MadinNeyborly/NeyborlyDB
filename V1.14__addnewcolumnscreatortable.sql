4bdda88d70b0afb7a345c2cbc067ae6c98fd1fd6
MadinNeyborly <97099242+MadinNeyborly@users.noreply.github.com>
2022-03-07 21:17:36 +0800

    added4newcolumns to creators table

---
+++ V1.14__addnewcolumnscreatortable.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.creators ADD slug text NULL;
+ALTER TABLE public.creators ADD description text NULL;
+ALTER TABLE public.creators ADD featuredimageurl text NULL;
+ALTER TABLE public.creators ADD social_links jsonb NULL;
